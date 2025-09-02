715a8f2acb0ab62afe80ce859b80e4f416792c6d
Anton <semianistyi.anton@lll.kpi.ua>
2025-09-02 20:07:19 +0300

    &&&

--- laba0/app.js
+++ laba0/app.js
@@ -1,3 +1,8 @@
 
 console.log("Haleo wordl");console.log("Haleo wordl");console.log("Haleo wordl");console.log("Haleo wordl");console.log("Haleo wordl");
 console.log("Haleo wordl");
+1
+22
+333
+4444
+55555
