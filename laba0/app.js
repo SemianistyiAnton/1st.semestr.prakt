31a118d803e9d9e74d75bcb7255eb846a0922bf2
Anton <semianistyi.anton@lll.kpi.ua>
2025-09-02 20:10:32 +0300

    first commit

--- laba0/app.js
+++ laba0/app.js
@@ -1,8 +1,1 @@
-
-console.log("Haleo wordl");console.log("Haleo wordl");console.log("Haleo wordl");console.log("Haleo wordl");console.log("Haleo wordl");
 console.log("Haleo wordl");
-1
-22
-333
-4444
-55555
